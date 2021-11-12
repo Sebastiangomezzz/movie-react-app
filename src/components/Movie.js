b5da52767976b1c0027e16a30ebbae2adc9adda6
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-11-13 00:58:08 +0100

    add MovieInfo and MovieInfoBar components and styling

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 //config
-import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 
 //components
-import Grid from './Grid';
-import Spinner from './Spinner';
-import BreadCrumb from './BreadCrumb';
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+import BreadCrumb from "./BreadCrumb";
+import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
 //hook
-import {useMovieFetch} from '../hooks/useMovieFetch'
+import { useMovieFetch } from "../hooks/useMovieFetch";
 //image
-import NoImage from '../images/no_image.jpg';
+import NoImage from "../images/no_image.jpg";
 
+const Movie = () => {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
 
-const Movie = ()=>{
-    const { movieId } = useParams();
-    const { state: movie, loading, error } = useMovieFetch(movieId);
-    
-    if (loading) return <Spinner/>
-    if (error) return <div>Something went wrong...</div>
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
 
-    return (
-        <>
-        <BreadCrumb 
-            movieTitle= {movie.original_title}
-        />
-            
-        </>
-    )
+  return (
+    <>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
+    </>
+  );
 };
 
 export default Movie;
