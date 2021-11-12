d64ed24f8ca98f842003639dc085e4347a0897cd
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-11-12 22:46:41 +0100

    add BreadCrumb and its styling, finished useMovieFetch hook

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,22 +1,31 @@
 import React from 'react';
-
+import { useParams } from 'react-router-dom';
 //config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 
 //components
 import Grid from './Grid';
 import Spinner from './Spinner';
-
+import BreadCrumb from './BreadCrumb';
 //hook
-
+import {useMovieFetch} from '../hooks/useMovieFetch'
 //image
 import NoImage from '../images/no_image.jpg';
 
 
 const Movie = ()=>{
+    const { movieId } = useParams();
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+    
+    if (loading) return <Spinner/>
+    if (error) return <div>Something went wrong...</div>
+
     return (
         <>
-            <div>Movie</div>
+        <BreadCrumb 
+            movieTitle= {movie.original_title}
+        />
+            
         </>
     )
 };
--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,8 +1,37 @@
 import { useState, useEffect } from "react";
 import API from '../API';
 
-export const useMovieFetch = () => {
+export const useMovieFetch = (movieId) => {
     const [state, setState] = useState({});
-    const [loading, setLoading] = useState(true);// este componente va a empezar haciendo un fetch así que loading va a ser true;
-    const []
-}
+    const [loading, setLoading] = useState(true);// este componente va a empezar haciendo un fetch así que loading va a ser true.
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchMovie = async () => {
+            try {
+                setLoading(true);
+                setError(false);
+
+                const movie = await API.fetchMovie(movieId);
+                const credits = await API.fetchCredits(movieId);
+
+                //get directors Only
+                const directors = credits.crew.filter(//crew es lo que recibimos y solo queremos los directores.
+                    member => member.job === 'Director'
+                );
+
+                setState({//creamos un objeto que tiene movie y su información, actors y directors t lo metemos en el estado.
+                    ...movie,
+                    actors: credits.cast, //aquí estamos metiendo los actores, que también están en el fetch de fetchCredits, que hemos asignado a la variable credits.
+                    directors: directors //le incluimos también los directores.
+                })
+                setLoading(false);
+            } catch (error) {
+                setError(true)
+            };
+        };
+        
+        fetchMovie()
+    }, [ movieId ])
+    return { state, loading, error };
+};
