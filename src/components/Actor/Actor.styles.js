0c81d9ae444f89d69ff091aa12b32bcdee3480fc
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-11-14 16:56:54 +0100

    add Actor component and styling

---
+++ src/components/Actor/Actor.styles.js
@@ -0,0 +1,17 @@
+import styled from './styled-components';
+
+export const Wrapper = styled.Wrapper`
+    color: var(--white);
+    background: var(--darkGrey);
+    border-radius: 20px;
+    padding: 5px;
+    text-align: center;
+
+    h3{
+        margin: 10px 0 0 0;
+    }
+`;
+
+export const Image = styled.img`
+
+`;
